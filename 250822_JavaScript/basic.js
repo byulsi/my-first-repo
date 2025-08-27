8472a4247173408295580888e034f02cfe46d668
MyeongJin <greenwoodjin@naver.com>
2025-08-27 17:54:35 +0900

    DOM을 배웠다.

---
+++ 250822_JavaScript/basic.js
@@ -0,0 +1,7 @@
+// 페이지가 모두 로드된 후 실행
+window.addEventListener('DOMContentLoaded', () => {
+  const greetingEl = document.getElementById('greeting');
+  greetingEl.textContent = 'VSCode Live Server 연결 완료!';
+});
+let a = 10;
+console.log(a);
